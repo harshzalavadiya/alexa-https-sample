cb625ad9feaf6927f1f50ac8e8eb2189d886e325
Harsh Zalavadiya <harsh.z@tcs.com>
2018-03-05 16:48:52 +0530

    Initial Code Push

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var http = require('http');
+var cheerio = require('cheerio');
+
+var responseJSON = {
+  "version": "1.0",
+  "sessionAttributes": {},
+  "response": {
+    "outputSpeech": {
+      "type": "SSML",
+      "ssml": "<speak>Whoops, I wasn\'t able to contact github!</speak>"
+    }
+  }
+}
+
+router.get('/', function(req, res, next) {
+	checkGithub(req, res, next);
+});
+
+router.post('/', function(req, res, next) {
+	checkGithub(req, res, next);
+});
+
+function checkGithub(req, res, next){
+	http.get({
+		host: 'status.github.com',
+		port: 443,
+		path: '/messages'
+	}, function(resp){
+		resp.on('data', function(chunk){
+			var $ = cheerio.load(chunk);
+			var lastStatus = $('#message-list').attr("data-last-known-status");
+			responseJSON.response.outputSpeech.ssml="<speak>according to status.github.com latest status was "+lastStatus+"</speak>"
+			res.send(responseJSON);
+		});
+	}).on("error", function(e){
+		res.send(responseJSON);
+		console.log("Got error: " + e.message);
+	});
+}
+
+module.exports = router;
