364a89a162be5de2985ff16854fa4084c1444714
Harsh Zalavadiya <zhnebula@gmail.com>
2018-03-05 17:00:20 +0530

    Update app.js
    Heroku Port Issue Fix

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-app.set('port', process.env.PORT || 5000);
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -44,7 +42,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-app.listen(app.get('port'));
 
 module.exports = app;
